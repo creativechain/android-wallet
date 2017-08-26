8fb65f00b34959f8e954d12efb0137fb2b763f04
Andersson <ander7agar@gmail.com>
2017-08-26 18:10:24 +0200

    new activity to check addresses

---
+++ app/src/main/java/crea/wallet/lite/ui/tool/CheckAddressActivity.java
@@ -0,0 +1,15 @@
+package crea.wallet.lite.ui.tool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import crea.wallet.lite.R;
+
+public class CheckAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_address);
+    }
+}
